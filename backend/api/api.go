6fbd85fdbfaff952b914232b877b6355ca7ab435
maxxfuu <maxfu826@gmail.com>
2024-12-06 18:05:04 -0800

    middleware

--- backend/api/api.go
+++ backend/api/api.go
@@ -4,22 +4,21 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/maxxfuu/To-Do-List/backend/middleware"
 )
 
 func SetUpRouter() *gin.Engine {
 	router := gin.Default()
 
 	// auth_handler.go
-	router.POST("/signin", postSignIn)            // *
-	router.POST("/signup", postSignUp)            // *
-	router.DELETE("/users/:username", deleteUser) // *
+	router.POST("/signin", postSignIn)
+	router.POST("/signup", postSignUp)
+	router.DELETE("/users/:username", deleteUser)
 
 	// task_handler.go
-	router.POST("/task", postTask)     //
-	router.PUT("/task", putTask)       //
-	router.DELETE("/task", deleteTask) //
-
-	// middleware.go
+	router.POST("/task", middleware.JWTAuthentication(), postTask)
+	router.PUT("/task", middleware.JWTAuthentication(), putTask)
+	router.DELETE("/task", middleware.JWTAuthentication(), deleteTask)
 
 	return router
 }
--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -1,24 +1,52 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+var secretKey = []byte("your-secret-key")
+
+// Middleware authentication method that validates JWT Token
 func JWTAuthentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		//Step 1: Extraction. Extract token from the Authorization Header.
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort() // Stop current middleware and prevent further handler function calls.
+			return
+		}
+
+		// Check if raw authHeader is valid.
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error: ": "Invalid authorization format"})
 			c.Abort()
 			return
 		}
 
-		tokenString := string.TrimPrefix(authHeader, "Bearer ")
-		if token == authHeader {
-			c.JSON(http.StatusUnauthorized, gin.H{"error: ": "Invalid authorization format"})
+		// Step 2: Parse and valiidate the token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return secretKey, nil
+		})
 
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
 		}
+
+		c.Next() // Allow access route handler if token is valid
+
 	}
 }
